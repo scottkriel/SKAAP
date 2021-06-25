109f183c54ecde436441440d2424fc74b38d7063
scottkriel <39770311+scottkriel@users.noreply.github.com>
2021-06-26 00:34:32 +0200

    Create detect_devices.py

---
+++ server/detect_devices.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 25 22:49:16 2021
+@author: Scott Kriel
+Description: Script to detect connected SDR devices
+"""
+import simplesoapy
+
+
+def detect_devices(soapy_args=''):
+    """Returns detected SoapySDR devices"""
+    devices = simplesoapy.detect_devices(soapy_args, as_string=True)
+    text = []
+    text.append('Detected SoapySDR devices:')
+    if devices:
+        for i, d in enumerate(devices):
+            text.append('  {}'.format(d))
+    else:
+        text.append('  No devices found!')
+    return (devices, '\n'.join(text))
+
+def main():
+    devices, devices_text = detect_devices()
+    print(devices_text)
+if __name__ == '__main__':
+    main()
+        
